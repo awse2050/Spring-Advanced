3339d1b73050ad97e235d541f000b663e0727e78
kyh <wesaq11@gmail.com>
2022-05-06 16:19:39 +0900

    complete V1

---
+++ src/main/java/hello/advanced/app/v1/OrderControllerV1.java
@@ -0,0 +1,37 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+    private final OrderServiceV1 orderService;
+
+    private final HelloTraceV1 trace;
+
+    /*
+        현재로서는 트랜잭션 ID의 동기화가 되지 않으며
+        로그의 Level 또한 조정되지 않는다.
+        v2 에서 파라미터를 통한 동기화를 진행한다.
+     */
+    @GetMapping("/v1/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+}
