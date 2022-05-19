7f310fdab34b78788fff3113c7e6c25d06893599
kyh <wesaq11@gmail.com>
2022-05-19 15:43:22 +0900

    Move Package

--- src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
+++ src/test/java/hello/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -4,8 +4,6 @@ import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 class ThreadLocalLogTraceTest {
 
--- src/test/java/hello/advanced/trace/threadlocal/code/FieldServiceTest.java
+++ src/test/java/hello/advanced/trace/threadlocal/code/FieldServiceTest.java
@@ -3,8 +3,6 @@ package hello.advanced.trace.threadlocal.code;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 class FieldServiceTest {
 
