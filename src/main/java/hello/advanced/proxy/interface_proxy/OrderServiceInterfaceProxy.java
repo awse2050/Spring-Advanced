c313b80c0e534473cbee11ea3b8c581edd4098ef
kyh <wesaq11@gmail.com>
2022-05-20 18:48:19 +0900

    adapt Proxy Pattern in App

---
+++ src/main/java/hello/advanced/proxy/interface_proxy/OrderServiceInterfaceProxy.java
@@ -0,0 +1,29 @@
+package hello.advanced.proxy.interface_proxy;
+
+import hello.advanced.proxy.v1.OrderServiceV1;
+import hello.advanced.proxy.v1.OrderServiceV1Impl;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceInterfaceProxy implements OrderServiceV1 {
+
+    private final OrderServiceV1Impl target;
+    private final LogTrace logTrace;
+
+    @Override
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderService.request()");
+            //target 호출
+            target.orderItem(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
